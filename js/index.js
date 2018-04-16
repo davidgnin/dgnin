3c1f05cdd02c7a43da2679d78873d9208ab8d4a3
David <me@davidgnin.com>
2018-04-16 21:04:28 +0200

    JS updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 'use strict';
+import smoothscroll from 'smoothscroll-polyfill';
 
-console.log('hello world');
+(function dgnin() {
+  let nav = document.querySelector('#web-nav ul');
+  let title = document.querySelector('#web-header h1');
+
+  let section = 'home';
+
+  smoothscroll.polyfill();
+  let changeSection = function changeSection(newSection) {
+    if (newSection !== section) {
+      section = newSection;
+      document.body.className = 'dg-' + newSection;
+      document.getElementById(newSection).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  };
+
+  nav.addEventListener('click', function (ev) {
+    let getEl = function (el) {
+      let name = el.tagName.toLowerCase();
+      if (name === 'li') {
+        return el;
+      } else if (name === 'ul') {
+        return false;
+      } else {
+        return getEl(el.parentNode);
+      }
+    };
+    let el = getEl(ev.target);
+    if (el) {
+      changeSection(el.className);
+    }
+  });
+
+  title.addEventListener('click', function () {
+    changeSection('home');
+  });
+})();
