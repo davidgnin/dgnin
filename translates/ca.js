4792289909228a6b19865be96b78dd1823d18bd5
David <me@davidgnin.com>
2018-04-26 12:32:16 +0200

    Contact section

--- translates/ca.js
+++ translates/ca.js
@@ -6,5 +6,7 @@ export default {
   curriculum: 'Currículum',
   contact: 'Contacte',
   homeH2: 'Hola, sóc en David i em dedico a programar software web.',
-  homeSubtitle: 'En aquest espai podràs veure a que em dedico i els projectes en els quals treballo.'
+  homeSubtitle: 'En aquest espai podràs veure a que em dedico i els projectes en els quals treballo.',
+  mailImage: 'Envia\'m un correu',
+  contactText: 'Si tens alguna pregunta, dubte o oferta, no dubtis en posar-te en contacte:'
 };
